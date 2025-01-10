54b6611dcebdf10dbff587b86e51d1e7993f8888
bruno.park <bruno.park@kakaocorp.com>
2025-01-10 12:54:54 +0900

    etc - 프로젝트 셋팅

---
+++ SwiftUIRedux/SwiftUIReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIReduxApp.swift
+//  SwiftUIRedux
+//
+//  Created by bruno on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
